6c11f257ec1541d47a87c2a2428699dd747aac07
kuba <jakubszrajber12@gmail.com>
2026-01-30 10:32:32 +0100

    new .gitignore

--- api/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.api_info, name="api_info"),
-    path("movies/", views.similar_movies, name="similar_movies"),
-    path("books/", views.similar_books, name="similar_books"),
-]
--- movies/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-
-def movies_recommendations_search(request):
-    return render(request, "movies/search_movie.html")
